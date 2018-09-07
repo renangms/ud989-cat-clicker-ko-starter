bf71239f9ae6c661f20311524f49a42e5e399271
Renan Machado <renangms@gmail.com>
2018-09-07 15:43:30 -0500

    Add more cats

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
+var cats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+        nicknames: ['Tabtab', 'Mr. T', 'T-Bone']
+    },
+    {
+        clickCount : 0,
+        name : 'Tiger',
+        imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+        nicknames: ['Tigger']
+    },
+    {
+        clickCount : 0,
+        name : 'Scaredy',
+        imgSrc : 'img/22252709_010df3379e_z.jpg',
+        nicknames: ['Casper']
+    },
+    {
+        clickCount : 0,
+        name : 'Shadow',
+        imgSrc : 'img/1413379559_412a540d29_z.jpg',
+        nicknames: ['Shooby']
+    },
+    {
+        clickCount : 0,
+        name : 'Sleepy',
+        imgSrc : 'img/9648464288_2516b35537_z.jpg',
+        nicknames: ['Zzzzz']
+    }
+]
+
 var Cat = function(data) {
     this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
@@ -19,15 +52,20 @@ var Cat = function(data) {
 var ViewModel = function() {
     var self = this;
 
-    this.cat = ko.observable(new Cat( {
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        nicknames: ['Tabtab', 'Mr. T', 'T-Bone']
-    }));
+    this.catList = ko.observableArray([]);
+
+    cats.forEach(function (cat) {
+        self.catList.push(new Cat(cat));
+    });
+
+    this.selectedCat = ko.observable(self.catList()[0]);
 
     this.incrementCounter = function() {
-        self.cat().clickCount(self.cat().clickCount() + 1);
+        self.selectedCat().clickCount(self.selectedCat().clickCount() + 1);
+    };
+
+    this.selectCat = function(clickedCat) {
+        self.selectedCat(clickedCat);
     };
 }
 
