b1e5ef4308a07e7f28f24b78f9ed489fa60cf024
Renan Machado <renangms@gmail.com>
2018-09-07 14:34:52 -0500

    Sepate model from viewmodel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
@@ -15,9 +15,13 @@ var ViewModel = function() {
 
     // this.nicknames = ko.observableArray(['Tabtab', 'Mr. T', 'T-Bone']);
     this.nicknames = ['Tabtab', 'Mr. T', 'T-Bone'];
+}
+
+var ViewModel = function() {
+    this.cat = ko.observable(new Cat());
 
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        this.cat().clickCount(this.cat().clickCount() + 1);
     };
 }
 
