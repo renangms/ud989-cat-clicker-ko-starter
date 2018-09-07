85496dac66f7e46ecdd044b63a650bdf6c17ba6a
Renan Machado <renangms@gmail.com>
2018-09-07 14:48:21 -0500

    Refactoring model to create custom cats

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.nicknames = ko.observableArray(data.nicknames);
+    // this.nicknames = data.nicknames;
 
     this.level = ko.computed(function () {
         if (this.clickCount() >= 10) {
@@ -12,15 +14,17 @@ var Cat = function() {
         }
 
     }, this);
-
-    // this.nicknames = ko.observableArray(['Tabtab', 'Mr. T', 'T-Bone']);
-    this.nicknames = ['Tabtab', 'Mr. T', 'T-Bone'];
 }
 
 var ViewModel = function() {
     var self = this;
 
-    this.cat = ko.observable(new Cat());
+    this.cat = ko.observable(new Cat( {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        nicknames: ['Tabtab', 'Mr. T', 'T-Bone']
+    }));
 
     this.incrementCounter = function() {
         self.cat().clickCount(self.cat().clickCount() + 1);
