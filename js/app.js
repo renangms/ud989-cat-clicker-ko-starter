51a537ee4f904bf053ca794504ef680d6143bc28
Renan Machado <renangms@gmail.com>
2018-09-07 14:16:02 -0500

    Add cat level

--- js/app.js
+++ js/app.js
@@ -3,6 +3,16 @@ var ViewModel = function() {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 
+    this.level = ko.computed(function () {
+        if (this.clickCount() >= 10) {
+            return 'Infant';
+        }
+        else {
+            return 'Newborn';
+        }
+
+    }, this);
+
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
     };
